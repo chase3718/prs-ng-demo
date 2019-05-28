ebbaad312e9ef7744de95e2d4a5347359b5d4896
Chase <chasewilliams98@gmail.com>
2019-05-28 08:51:18 -0400

    updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prs-ng-demo';
+  title = 'JavaSampleApproach';
+  description = 'Angular4-SpringBoot Demo';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+ 
+import {AppComponent} from './app.component';
+import {DetailsUploadComponent} from './upload/details-upload/details-upload.component';
+import {FormUploadComponent} from './upload/form-upload/form-upload.component';
+import {ListUploadComponent} from './upload/list-upload/list-upload.component';
+import {UploadFileService} from './upload/upload-file.service';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsUploadComponent,
+    FormUploadComponent,
+    ListUploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UploadFileService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
